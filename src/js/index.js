ef0529d531d58ee31e6c2354c571b316e5256838
Duduxs <duduxss3@gmail.com>
2020-08-02 18:17:00 -0300

    reset.css, iframe and function clicked added, footer first implementation

---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+const section = document.querySelector('#frame-spec');
+
+function clicked(id) {
+
+    switch (id) {
+        case 1:
+           section.src = './index-info.html#about';
+            break;
+        case 2:
+            section.src = './index-info.html#skills';
+            break;
+        case 3:
+            section.src = './index-info.html#projects';
+            break;
+        case 4:
+            section.src = './index-info.html#languages';
+            break;
+    }
+ 
+}
